5046f4e519ae4a50e8c11ec4418ad2ad7597f2df
Moremilk <moremilksut@gmail.com>
2015-01-02 19:41:24 +0700

    Week 10

--- app/platforms/android/assets/www/js/controller.js
+++ app/platforms/android/assets/www/js/controller.js
@@ -1,46 +1,5 @@
 var serverUrl = 'http://localhost:2222'
 
-app.controller('AppController', function($scope, $http) {
-	$scope.map = { 
-		center: { 
-			latitude: 14.9047824,
-			longitude: 102.0107774
-		}, 
-		zoom: 15,
-		draggable: true
-	}
-
-	$scope.marker = {
-		options : {
-			draggable: false
-		}
-	}
-
-	$scope.test = function() {
-		alert('click')
-	}
-
-	$scope.windowOptions = {
-        visible: true
-    };
-
-    $scope.onClick = function() {
-        $scope.windowOptions.visible = !$scope.windowOptions.visible
-    }
-
-    $scope.closeClick = function() {
-        $scope.windowOptions.visible = false
-    }
-
-	$scope.rewardslist = function() {
-		$http.get(serverUrl + '/rewards/list').success(function(data) {
-			ons.notification.alert({
-				message: 'เมื่อซื้อครบ '+ data.total +' บาทจะได้รับ '+ data.point +' แต้ม'
-			})
-		})
-	}
-})
-
 /* user, admin : Login */
 app.controller('LoginController', function($scope, $http, Share) {
 
@@ -111,38 +70,6 @@ app.controller('UserManageController', function($scope, $http, $filter, Share) {
 	        adminNav.popPage()
 	    })
 	}
-})
-
-/* user : reg new */
-app.controller('RegController', function($scope, $http) {
- 
-  	$scope.save = function() {
-    	var p1 = $scope.password1
-      	var p2 = $scope.password2
-
-      	if(p1 == p2) {
-        	var password = p1
-        	var data = {
-          		'name'    	: $scope.name,
-          		'lastname'  : $scope.lastname,
-          		'tel'     	: $scope.tel,
-          		'birthday'  : $scope.birthday,
-          		'gender'  	: $scope.gender,
-          		'email'   	: $scope.email,
-          		'password'  : password,
-          		'type'    	: 'USER'
-      		}
-
-        	console.log(data)
-      
-        	$http.post(serverUrl + '/user/reg', data).success(function(data) {
-          		alert(data)
-          		myNav.pushPage('index.html')
-        	})
-      	} else {
-        	alert('pw not wrong')
-      	}
-  	}
 })
 
 /* admin : give & condition rewards */
@@ -271,53 +198,6 @@ app.controller('ReportController', function($scope, $http) {
 	function callback(data) {
 		$scope.datas = data
 	}
-})
-
-/* user : data of user */
-app.controller('UserController', function($scope, $http, $filter, Share) {
-	$scope.listRewards = function() {
-		$http.get(serverUrl + '/userrewards/' + Share.getTel()).success(function(data) {
-			$scope.userrewards = data
-			$scope.update = $filter('date')(data.update, 'yyyy-MM-dd HH:mm:ss')
-		})
-	}
-	$scope.listRewards()
-
-	$scope.listuser = function() {
-		$http.get(serverUrl + '/user/list/' + Share.getTel()).success(function(data) {
-			$scope.user = data
-			$scope.date = new Date($filter('date')(data.birthday, 'longDate'))
-		})
-	}
-	$scope.listuser()
-
-	$scope.edit = function() {
-		var tel = Share.getTel()
-		var data = {
-			'name'    	: $scope.user.name,
-          	'lastname'  : $scope.user.lastname,
-          	'tel'     	: $scope.user.tel,
-          	'birthday'  : $scope.date,
-          	'gender'  	: $scope.user.gender,
-          	'email'   	: $scope.user.email,			
-		}
-		console.log(data)
-
-		$http.post(serverUrl + '/user/update/' + tel, data).success(function(data) {
-			//alert(data)
-			menu.setMainPage('user/settings.html')
-		})
-	}
-})
-
-/* user : show news */
-app.controller('NewsController', function($scope, $http) {
-	$scope.newslist = function() {
-		$http.get(serverUrl + '/news/list').success(function(data) {
-			$scope.datas = data
-		})
-	}	
-	$scope.newslist()
 })
 
 /* admin : manage news */
